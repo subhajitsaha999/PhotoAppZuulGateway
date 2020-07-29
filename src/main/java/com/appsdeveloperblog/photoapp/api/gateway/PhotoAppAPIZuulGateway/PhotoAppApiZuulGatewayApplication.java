d030ff9dfaaa9419dcddd20ae14b06b3e059edf6
subhajitsaha999 <subhajit.phscs.saha@gmail.com>
2020-07-29 20:14:03 +0530

    first commit

---
+++ src/main/java/com/appsdeveloperblog/photoapp/api/gateway/PhotoAppAPIZuulGateway/PhotoAppApiZuulGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.appsdeveloperblog.photoapp.api.gateway.PhotoAppAPIZuulGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class PhotoAppApiZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiZuulGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appsdeveloperblog/photoapp/api/gateway/PhotoAppAPIZuulGateway/PhotoAppApiZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.api.gateway.PhotoAppAPIZuulGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiZuulGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
